16a200d38909ab1b51edf9a0dc12af962a7e4c03
kame <midorima570@gmail.com>
2021-08-20 15:46:13 +0900

    タイムゾーン修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname='.$_ENV['DB_NAME'].';host='.$_ENV['HOST_NAME'].';charset=utf8', $_ENV['USER_NAME'], $_ENV['PASSWORD']);
+    $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
+    $db = new PDO('mysql:dbname='.$_ENV['DB_NAME'].';host='.$_ENV['HOST_NAME'].';charset=utf8', $_ENV['USER_NAME'], $_ENV['PASSWORD'], $driver_options);
 } catch (PDOException $e) {
     echo 'DB接続エラー' . $e->getMessage();
 }
